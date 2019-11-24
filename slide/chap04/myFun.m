c57107c1fb2877870db84afcb6102c2a08725584
Mifta Nur Farid <miftanurfarid@gmail.com>
2019-11-24 10:47:08 +0800

    menghitung plotting frekuensi kompleks

---
+++ slide/chap04/myFun.m
@@ -0,0 +1,18 @@
+function vt = myFun(vm, omega, t, theta, sigma)
+%myFun - Description
+%
+% Syntax: vt = myFun(vm, omega, t, theta, sigma)
+%
+% Melakukan perhitungan frekuensi kompleks kemudian
+% melakukan plotting grafik responnya.
+% 
+% Input : 
+% vm    : Tegangan Maksimum (volt)
+% omega : Frekuensi sudut (rad / s)
+% t     : Waktu (s)
+% theta : Fasa (rad)
+% sigma : Frekuensi neper
+    
+% Fungsi Sinusoidal
+    vt1 = vm * cos()
+end
